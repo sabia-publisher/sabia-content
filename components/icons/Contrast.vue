de8d278bfc7b50af8d541d2c8e7daa0bdce2c70a
Eduardo Camillo <eduardo.ckf@gmail.com>
2022-10-08 16:01:45 -0300

    chore: remove unused components

--- components/icons/Contrast.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  	<svg class="w-6 h-6" fill="currentColor" stroke="none" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 17 17">
-		<path d="M9,16.2c-4.4,0-8-3.6-8-8s3.6-8,8-8s8,3.6,8,8S13.4,16.2,9,16.2z M9,1.2c-3.9,0-7,3.1-7,7c0,3.9,3.1,7,7,7
-			c3.9,0,7-3.1,7-7C16,4.4,12.8,1.2,9,1.2z"/>
-		<path d="M9,14.2c3.3,0,6-2.7,6-6c0-3.3-2.7-6-6-6V14.2z"/>
-	</svg>
-</template>
