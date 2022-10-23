b0ec70edb65b07fa942568d87ef57d55a25f3159
Eduardo Camillo <eduardo.ckf@gmail.com>
2022-10-23 15:25:46 -0300

    feat: redirecto to website on homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,8 @@
 <script setup>
-
+onBeforeMount(() => {
+    window.location.href = 'https://sabia.pub/'
+})
 </script>
 
 <template>
-    <div>
-        <p><NuxtLink to="/9786599492907/prefacio">Julio</NuxtLink></p>
-        <p><NuxtLink to="/9786599492938/gratitudes">Clara</NuxtLink></p>
-    </div>
 </template>
